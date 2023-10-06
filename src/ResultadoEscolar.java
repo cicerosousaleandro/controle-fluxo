7fb3d3411d3e2dec6bd3c0539ddf14b488c339e5
Cicero Sousa <cicerosousa.le@gmail.com>
2023-10-05 22:54:15 -0300

    update de condição ternária

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -3,17 +3,9 @@ public class ResultadoEscolar {
 
 	public static void main(String[] args) {
 		
-		int nota = 6;
-		
-		if(nota >= 7)
-			System.out.println("Aprovado!");
-		
-		else if (nota >=5 && nota <7)
-			System.out.println("Prova de recuperação");
-			
-		else
-			System.out.println("Reprovado!");
-
+		int nota = 7;
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
 	}
 
 }
