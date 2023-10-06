a4e7dbac6d8063e0946e1b6deb606acbe4925643
Cicero Sousa <cicerosousa.le@gmail.com>
2023-10-05 22:30:29 -0300

    Update da nova aula de estrutura condicional

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -3,11 +3,14 @@ public class ResultadoEscolar {
 
 	public static void main(String[] args) {
 		
-		int nota = 7;
+		int nota = 6;
 		
 		if(nota >= 7)
 			System.out.println("Aprovado!");
 		
+		else if (nota >=5 && nota <7)
+			System.out.println("Prova de recuperação");
+			
 		else
 			System.out.println("Reprovado!");
 
